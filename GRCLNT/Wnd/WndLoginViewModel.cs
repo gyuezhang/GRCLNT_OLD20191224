9799680c3b9a1556cd77957348ff2310ff17004f
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-13 17:54:35 +0800

    Login界面

--- GRCLNT/Wnd/WndLoginViewModel.cs
+++ GRCLNT/Wnd/WndLoginViewModel.cs
@@ -6,5 +6,26 @@ namespace GRCLNT.Wnd
 {
     public class WndLoginViewModel : Screen
     {
+        public int iTransitionerIndex { get; set; } = 0;
+
+        public void ShowSetting()
+        {
+            iTransitionerIndex = 1;
+        }
+
+        public void TestServer()
+        {
+            iTransitionerIndex = 0;
+        }
+
+        public void SaveSetting()
+        {
+            iTransitionerIndex = 0;
+        }
+
+        public void ExitLogin()
+        {
+            this.RequestClose();
+        }
     }
 }
