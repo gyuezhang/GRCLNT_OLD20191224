7a470834c6a9af2a2dd24e48f928df1d1c4744af
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-15 16:29:20 +0800

    窗口状态处理

--- GRCLNT/Wnd/WndMainViewModel.cs
+++ GRCLNT/Wnd/WndMainViewModel.cs
@@ -4,6 +4,7 @@ using GRCLNT;
 using GRCLNT.Socket;
 using MaterialDesignThemes.Wpf;
 using System.Net;
+using System.Windows;
 
 namespace GRCLNT.Wnd
 {
@@ -13,6 +14,24 @@ namespace GRCLNT.Wnd
         public WndMainViewModel(IWindowManager windowManager)
         {
             this.windowManager = windowManager;
+            dbMaxHeight = SystemParameters.WorkArea.Height + 7;
+            dbMaxWidth = SystemParameters.WorkArea.Width + 7;
+        }
+        public WindowState CurWindowState { get; set; }
+        public double dbMaxHeight { get; set; }
+        public double dbMaxWidth { get; set; }
+
+        public Thickness bdMargin { get; set; } = new Thickness(0, 0, 0, 0);
+        public Visibility vImageDragVisible { get; set; } = Visibility.Visible;
+
+        public void OnStateChanged()
+        {
+            //重置最大窗口尺寸（此处避免运行过程中任务栏显隐）
+            dbMaxHeight = SystemParameters.WorkArea.Height + 7;
+            dbMaxWidth = SystemParameters.WorkArea.Width + 7;
+            bdMargin = (CurWindowState == WindowState.Maximized) ? new Thickness(7, 7, 0, 0) : new Thickness(0, 0, 0, 0);
+
+            vImageDragVisible = (CurWindowState != WindowState.Maximized)? Visibility.Visible:Visibility.Hidden;
         }
     }
 }
