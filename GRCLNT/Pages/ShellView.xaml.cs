9ad1fcae93bf8ae96d6e719776368ba4273ef6cb
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-17 21:55:52 +0800

    添加MVVM依赖
    Install-Package Stylet.Start
    Install-Package PropertyChanged.Fody

---
+++ GRCLNT/Pages/ShellView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace GRCLNT.Pages
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- GRCLNT/View/Wnd/WndMain.xaml.cs
+++ GRCLNT/View/Wnd/WndMain.xaml.cs
@@ -18,9 +18,9 @@ namespace GRCLNT
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WndMain : Window
     {
-        public MainWindow()
+        public WndMain()
         {
             InitializeComponent();
         }
