ddf3f27df55f334024ae6497257e625e76d26533
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-18 23:10:02 +0800

    开始写 socket client

--- Socket/CLNTClient.cs
+++ Socket/CLNTClient.cs
@@ -6,5 +6,13 @@ namespace Socket
 {
     public class CLNTClient
     {
+        private static EasyClient ezClient;
+        private static string Ip;
+        private static string Port;
+
+        public static void Init(string ip)
+        {
+
+        }
     }
 }
