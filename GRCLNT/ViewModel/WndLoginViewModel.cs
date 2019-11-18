6b812d58822bcaeda08cfbaa74fbaff9b4a003db
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-18 22:25:52 +0800

    login 界面逻辑

--- GRCLNT/ViewModel/WndLoginViewModel.cs
+++ GRCLNT/ViewModel/WndLoginViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
 using Models;
 using Stylet;
 
@@ -6,6 +8,60 @@ namespace GRCLNT
 {
     public class WndLoginViewModel : Screen
     {
-        public CfgLogin login { get; set; } = new CfgLogin();
+        public WndLoginViewModel()
+        {
+
+        }
+
+        public CfgLogin loginCfg { get; set; } = Cfg.GetLogin();
+
+        public void OnStartLogin()
+        {
+            LoginSuccess();
+        }
+
+        public void OnSettingOK()
+        {
+            iTransitionerIndex = 0;
+        }
+
+        public void OnTestServer()
+        {
+            iTransitionerIndex = 0;
+        }
+
+        public void OnShowSetting()
+        {
+            iTransitionerIndex = 1;
+        }
+
+        public void OnExit()
+        {
+            this.RequestClose();
+        }
+
+        public void LoginSuccess()
+        {
+            Cfg.SetLogin(loginCfg);
+        }
+
+        #region 窗口状态，用于修复双击窗口会移动位置的bug
+        public WindowState loginWindowState { get; set; }
+        public void OnStateChanged()
+        {
+            if (loginWindowState == WindowState.Maximized)
+            {
+                loginWindowState = WindowState.Normal;
+            }
+        }
+        #endregion
+
+        public int iTransitionerIndex { get; set; } = 0;
+
+        
+        #region 提示消息
+        public SnackbarMessageQueue loginMessageQueue { get; set; } = new SnackbarMessageQueue();
+        
+        #endregion
     }
 }
