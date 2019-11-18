57e73fc9bcab814858fa37c391a4745be6410aba
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-18 17:29:31 +0800

    wpf引入 stylet

--- GRCLNT/Pages/ShellView.xaml.cs
+++ GRCLNT/Pages/ShellView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GRCLNT
+namespace GRCLNT.Pages
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShellView : Window
     {
-        public MainWindow()
+        public ShellView()
         {
             InitializeComponent();
         }
---
+++ GRCLNT/Pages/ShellViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Stylet;
+
+namespace GRCLNT.Pages
+{
+    public class ShellViewModel : Screen
+    {
+    }
+}
