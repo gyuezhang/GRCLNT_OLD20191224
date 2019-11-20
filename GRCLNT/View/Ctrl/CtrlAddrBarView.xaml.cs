1149babbcfa8a0ed873add7729aafb7f4f18d0a9
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-21 01:40:49 +0800

    添加了一个假的地址栏

---
+++ GRCLNT/View/Ctrl/CtrlAddrBarView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GRCLNT
+{
+    /// <summary>
+    /// CtrlAddrBarView.xaml 的交互逻辑
+    /// </summary>
+    public partial class CtrlAddrBarView : UserControl
+    {
+        public CtrlAddrBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
