1f77846fc731c40e055b7dd3a2a70ee0a8f58885
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-20 17:26:00 +0800

    wndmainview basic

--- GRCLNT/ViewModel/WndMainViewModel.cs
+++ GRCLNT/ViewModel/WndMainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Stylet;
 using System;
+using System.Windows;
 
 namespace GRCLNT
 {
@@ -10,6 +11,29 @@ namespace GRCLNT
         public WndMainViewModel(IWindowManager windowManager)
         {
             _windowManager = windowManager;
+            dbMaxHeight = SystemParameters.WorkArea.Height + 7;
+            dbMaxWidth = SystemParameters.WorkArea.Width + 7;
+        }
+
+        public WndMainViewModel()
+        {
+
+        }
+        public WindowState CurWindowState { get; set; }
+        public double dbMaxHeight { get; set; } = SystemParameters.WorkArea.Height + 7;
+        public double dbMaxWidth { get; set; } = SystemParameters.WorkArea.Width + 7;
+
+        public Thickness bdMargin { get; set; } = new Thickness(0, 0, 0, 0);
+        public Visibility vImageDragVisible { get; set; } = Visibility.Visible;
+
+        public void OnStateChanged()
+        {
+            //重置最大窗口尺寸（此处避免运行过程中任务栏显隐）
+            dbMaxHeight = SystemParameters.WorkArea.Height + 7;
+            dbMaxWidth = SystemParameters.WorkArea.Width + 7;
+            bdMargin = (CurWindowState == WindowState.Maximized) ? new Thickness(7, 7, 0, 0) : new Thickness(0, 0, 0, 0);
+
+            vImageDragVisible = (CurWindowState != WindowState.Maximized) ? Visibility.Visible : Visibility.Hidden;
         }
 
         public void OnLogOut()
