fa5131adabbcf8be04375440c3943d1973c37a1e
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-22 01:02:38 +0800

    Update UI_Def.cs

--- GRCLNT/View/Resource/Tool/UI_Def.cs
+++ GRCLNT/View/Resource/Tool/UI_Def.cs
@@ -7,5 +7,29 @@ using System.Threading.Tasks;
 namespace GRCLNT
 {
     //定义页面及子页面
+    public enum EnumPage
+    {
+        Home,
+            Home_Dashboard,
+            Home_UsrInfo,
+                Home_UsrInfo_ChangeInfo,
+                Home_UsrInfo_ChangPwd,
 
+        Well,
+            Well_AddMtdSel,
+                Well_AddEdit,
+                Well_AddAuto,
+            Well_Search,
+            Well_State,
+            Well_Output,
+            Well_Setting,
+
+        EntWell,
+        SediCtrl,
+        GwDyna,
+        GwProj,
+        Hydro,
+        Law,
+        Setting,
+    }
 }
