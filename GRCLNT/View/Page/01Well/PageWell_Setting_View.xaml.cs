e58022de9fce9af9c0902dc61a3f511c70f4a8dc
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-02 14:47:15 +0800

    well settting list鼠标响应

--- GRCLNT/View/Page/01Well/PageWell_Setting_View.xaml.cs
+++ GRCLNT/View/Page/01Well/PageWell_Setting_View.xaml.cs
@@ -11,5 +11,37 @@ namespace GRCLNT
         {
             InitializeComponent();
         }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private void ListViewLoc_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            ListView lv = (ListView)sender;
+            double d = lv.ActualHeight;
+            svLoc.ScrollToVerticalOffset(d);
+        }
+        private void ListViewTubeMat_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            ListView lv = (ListView)sender;
+            double d = lv.ActualHeight;
+            svTubeMat.ScrollToVerticalOffset(d);
+        }
+        private void ListViewPumpModel_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            ListView lv = (ListView)sender;
+            double d = lv.ActualHeight;
+            svPumpModel.ScrollToVerticalOffset(d);
+        }
+        private void ListViewUnitCat_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            ListView lv = (ListView)sender;
+            double d = lv.ActualHeight;
+            svUnitCat.ScrollToVerticalOffset(d);
+        }
     }
 }
