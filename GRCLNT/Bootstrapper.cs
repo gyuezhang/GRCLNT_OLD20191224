3c1c1a722a131987424d9a841ba683e0a47308c4
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-16 23:08:16 +0800

    frame

---
+++ GRCLNT/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Stylet;
+using StyletIoC;
+using GRCLNT.Pages;
+
+namespace GRCLNT
+{
+    public class Bootstrapper : Bootstrapper<ShellViewModel>
+    {
+        protected override void ConfigureIoC(IStyletIoCBuilder builder)
+        {
+            // Configure the IoC container in here
+        }
+
+        protected override void Configure()
+        {
+            // Perform any other configuration before the application starts
+        }
+    }
+}
